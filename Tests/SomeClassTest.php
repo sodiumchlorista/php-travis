77bfe50523bc9f03c078d133767ac1f351105060
sodiumchlorista <odnivas@icloud.com>
2020-01-21 02:34:34 +0300

    testcase phpunit 7.0

--- Tests/SomeClassTest.php
+++ Tests/SomeClassTest.php
@@ -7,14 +7,14 @@ class SomeClassTest extends TestCase
      */
     private $pdo;
 
-    public function setUp()
+    public function setUp(): void
     {
         $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
     }
 
-    public function tearDown()
+    public function tearDown(): void
     {
         $this->pdo->query("DROP TABLE hello");
     }
